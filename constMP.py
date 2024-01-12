f0f60f102ca49fe8a0355e0fa3728fd0386f7c78
Gabriel Oliveira <gabriel.oliveira@irancho.com.br>
2024-01-12 20:14:28 -0300

    add novas vms

--- constMP.py
+++ constMP.py
@@ -5,7 +5,7 @@
 
 # With public addresses (in the same region of the cloud)
 # The last one is not fixed and must be changed each time the lab is restarted.
-PEERS_SAME_REGION = ['100.25.72.80','23.23.123.132'] ## ,'34.235.105.7','50.17.58.135','54.209.66.27','100.25.166.32']
+PEERS_SAME_REGION = ['100.25.72.80','23.23.123.132', '34.235.105.7', '50.17.58.135', '54.209.66.27'] ## ,'34.235.105.7','50.17.58.135','54.209.66.27','100.25.166.32']
 
 # With public addresses (in two separate regions - last two servers in Oregon)
 PEERS_TWO_REGIONS = ['3.219.23.96','34.226.166.158','44.198.151.245','44.219.197.47','44.239.194.163','52.88.79.71']
@@ -18,7 +18,7 @@ SERVER_ADDR ='100.25.72.80'
 SERVER_PORT = 5678
 
 # Number of valid operations to call
-NUM_OPS = 2
+NUM_OPS = 5
 
 # List of operations
 ops = ['deposit', 'interest']
